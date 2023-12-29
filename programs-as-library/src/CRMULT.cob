7c1380a071dc7398408dfc8087ee8bd82e63201e
Peeter Joot <peeterjoot@pm.me>
2023-12-29 21:23:21 +0100

    CRMULT

--- programs-as-library/src/CRMULT.cob
+++ programs-as-library/src/CRMULT.cob
@@ -4,9 +4,16 @@
       *CONFIGURATION SECTION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+        COPY FLOAT REPLACING ==(PRFX)== BY ==WS-MULT-TMP-RE-==.
+        COPY FLOAT REPLACING ==(PRFX)== BY ==WS-MULT-TMP-IM-==.
        LINKAGE SECTION.
+        COPY COMPLEX REPLACING ==(PRFX)== BY ==LK-MULTPARM-IN1-==.
+        COPY COMPLEX REPLACING ==(PRFX)== BY ==LK-MULTPARM-IN2-==.
+        COPY COMPLEX REPLACING ==(PRFX)== BY ==LK-MULTPARM-OUT-==.
        PROCEDURE DIVISION USING
-        LK-ADDPARM-IN1-COMPLEX.
+        LK-MULTPARM-IN1-COMPLEX,
+        LK-MULTPARM-IN2-COMPLEX,
+        LK-MULTPARM-OUT-COMPLEX.
       ******************************************************************
       * LIBRARY ROUTINE: COMPLEX-MULT
       *
@@ -19,13 +26,17 @@
       *
       *   (a + b i)(c + d i) = a c - b d + i( b c + a d)
        COMPLEX-MULT.
-           COMPUTE LK-MULTPARM-OUT-RE =
+           COMPUTE WS-MULT-TMP-RE-V =
              (LK-MULTPARM-IN1-RE * LK-MULTPARM-IN2-RE) -
              (LK-MULTPARM-IN1-IM * LK-MULTPARM-IN2-IM)
 
-           COMPUTE LK-MULTPARM-OUT-IM =
+           COMPUTE WS-MULT-TMP-IM-V =
              (LK-MULTPARM-IN1-IM * LK-MULTPARM-IN2-RE) +
              (LK-MULTPARM-IN1-RE * LK-MULTPARM-IN2-IM)
+
+      * temps in case there's aliasing:
+           MOVE WS-MULT-TMP-RE-V TO LK-MULTPARM-OUT-RE
+           MOVE WS-MULT-TMP-IM-V TO LK-MULTPARM-OUT-IM
 
            GOBACK
            .
