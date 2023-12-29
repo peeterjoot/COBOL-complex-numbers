0849d2b6c7411455ced6d5d52be8b06a91321033
Peeter Joot <peeter.joot@lzlabs.com>
2023-12-29 19:01:55 +0100

    CSUB

--- programs-as-library/src/CSUB.cob
+++ programs-as-library/src/CSUB.cob
@@ -5,23 +5,29 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        LINKAGE SECTION.
-       PROCEDURE DIVISION.
+        COPY COMPLEX REPLACING ==(PRFX)== BY ==LK-SUBPARM-IN1-==.
+        COPY COMPLEX REPLACING ==(PRFX)== BY ==LK-SUBPARM-IN2-==.
+        COPY COMPLEX REPLACING ==(PRFX)== BY ==LK-SUBPARM-OUT-==.
+       PROCEDURE DIVISION USING
+        LK-SUBPARM-IN1-COMPLEX,
+        LK-SUBPARM-IN2-COMPLEX,
+        LK-SUBPARM-OUT-COMPLEX.
       ******************************************************************
       * LIBRARY ROUTINE: COMPLEX-SUB
       *
-      * @param [in] WS-SUBPARM-IN1-COMPLEX,
-      *   with members WS-SUBPARM-IN1-RE, WS-SUBPARM-IN1-IM.
-      * @param [in] WS-SUBPARM-IN2-COMPLEX,
-      *   with members WS-SUBPARM-IN2-RE, WS-SUBPARM-IN2-IM.
-      * @param [out] WS-SUBPARM-OUT-COMPLEX,
-      *   with members WS-SUBPARM-OUT-RE, WS-SUBPARM-OUT-IM.
+      * @param [in] LK-SUBPARM-IN1-COMPLEX,
+      *   with members LK-SUBPARM-IN1-RE, LK-SUBPARM-IN1-IM.
+      * @param [in] LK-SUBPARM-IN2-COMPLEX,
+      *   with members LK-SUBPARM-IN2-RE, LK-SUBPARM-IN2-IM.
+      * @param [out] LK-SUBPARM-OUT-COMPLEX,
+      *   with members LK-SUBPARM-OUT-RE, LK-SUBPARM-OUT-IM.
       *
        COMPLEX-SUB.
-           COMPUTE WS-SUBPARM-OUT-RE =
-             WS-SUBPARM-IN1-RE - WS-SUBPARM-IN2-RE
+           COMPUTE LK-SUBPARM-OUT-RE =
+             LK-SUBPARM-IN1-RE - LK-SUBPARM-IN2-RE
 
-           COMPUTE WS-SUBPARM-OUT-IM =
-             WS-SUBPARM-IN1-IM - WS-SUBPARM-IN2-IM
+           COMPUTE LK-SUBPARM-OUT-IM =
+             LK-SUBPARM-IN1-IM - LK-SUBPARM-IN2-IM
 
            GOBACK
            .
