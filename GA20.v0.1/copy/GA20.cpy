1670bf2d18329ac43cab18a241271a5560cf5229
Peeter Joot <peeter.joot@lzlabs.com>
2023-12-31 05:05:29 +0100

    Add GAMSCAL.cob

--- GA20.v0.1/copy/GA20.cpy
+++ GA20.v0.1/copy/GA20.cpy
@@ -2,5 +2,6 @@
       * copy/GA20.cpy
        01 GA-DISPLAY          PIC X(20) VALUE 'GADISP'.
        01 GA-MULT-RETURN      PIC X(20) VALUE 'GARMULT'.
+       01 GA-MKSCALAR-MODIFY  PIC X(20) VALUE 'GAMSCAL'.
       *01 GA-MULT-MODIFY      PIC X(20) VALUE 'GAMMULT'.
       ******************************************************************
--- GA20.v0.1/src/GADISP.cob
+++ GA20.v0.1/src/GADISP.cob
@@ -2,6 +2,8 @@
        PROGRAM-ID.    GADISP.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+        01 WS-PLUS PIC XX VALUE '  '.
+        COPY GADISP REPLACING ==(PRFX)== BY ==WS-PREFIX-==.
        LINKAGE SECTION.
         COPY GADISP REPLACING ==(PRFX)== BY ==L-IN-==.
         COPY MV REPLACING ==(PRFX)== BY ==L-IN-==.
@@ -12,12 +14,51 @@
       * @param [in] L-IN-GA20,
       *   with members ...
       *
-           DISPLAY L-IN-N ' GRADE: ' L-IN-GRADE
-           DISPLAY L-IN-N ' 0: ' L-IN-SC
-           DISPLAY L-IN-N ' 1: (' L-IN-X ', ' L-IN-Y ')'
-           DISPLAY L-IN-N ' 2: ' L-IN-PS
+           MOVE L-IN-N TO WS-PREFIX-N
+
+           EVALUATE L-IN-GRADE
+             WHEN 0 PERFORM DISPLAY-GRADE-0
+             WHEN 1 PERFORM DISPLAY-GRADE-1
+             WHEN 2 PERFORM DISPLAY-GRADE-2
+             WHEN OTHER PERFORM DISPLAY-MV
+           END-EVALUATE
 
            GOBACK
+           .
+
+        DISPLAYED-NONZERO.
+           MOVE '+ ' TO WS-PLUS
+           MOVE SPACES TO WS-PREFIX-N
+           .
+        DISPLAY-GRADE-0.
+           IF L-IN-SC NOT = 0
+             DISPLAY WS-PREFIX-N '(' L-IN-SC ')'
+             PERFORM DISPLAYED-NONZERO
+           END-IF
+           .
+        DISPLAY-GRADE-1.
+           IF L-IN-X NOT = 0
+             IF L-IN-Y NOT = 0
+               DISPLAY WS-PREFIX-N WS-PLUS 
+                 '( ' L-IN-X ') e_1 + (' L-IN-Y ') e_2'
+               PERFORM DISPLAYED-NONZERO
+             END-IF
+           ELSE IF L-IN-Y NOT = 0
+             DISPLAY WS-PREFIX-N '(' L-IN-Y ') e_2'
+             PERFORM DISPLAYED-NONZERO
+           END-IF
+           .
+        DISPLAY-GRADE-2.
+           IF L-IN-PS NOT = 0
+             DISPLAY WS-PREFIX-N WS-PLUS '(' L-IN-PS ') e_{12}'
+             PERFORM DISPLAYED-NONZERO
+           END-IF
+           .
+        DISPLAY-MV.
+           PERFORM DISPLAY-GRADE-0 THROUGH DISPLAY-GRADE-2
+           IF WS-PLUS = SPACES
+             DISPLAY WS-PREFIX-N 0
+           END-IF
            .
 
       * vim: et ts=4 sw=4
