5c6e31acc831832388f0c32c61d5094d82ac276f
Peeter Joot <peeter.joot@lzlabs.com>
2023-12-31 06:13:36 +0100

    One test case for GA-MULT-RETURN

--- GA20.v0.1/src/MULT.cob
+++ GA20.v0.1/src/MULT.cob
@@ -8,6 +8,8 @@
        COPY MV REPLACING ==(PRFX)== BY ==B-==.
        COPY MV REPLACING ==(PRFX)== BY ==C-==.
        COPY MV REPLACING ==(PRFX)== BY ==D-==.
+       COPY MV REPLACING ==(PRFX)== BY ==E-==.
+       COPY MV REPLACING ==(PRFX)== BY ==ONE-==.
        COPY GADISP REPLACING ==(PRFX)== BY ==WS-DISPPARM-==.
        PROCEDURE DIVISION.
 
@@ -17,6 +19,7 @@
            MOVE 3 TO A-SC
            MOVE 4 TO A-PS
 
+           CALL GA-MKSCALAR-MODIFY USING ONE-MV, A-X
            CALL GA-MKSCALAR-MODIFY USING B-MV, A-SC
            CALL GA-MKVECTOR-MODIFY USING C-MV, A-X, A-Y
            CALL GA-MKPSEUDO-MODIFY USING D-MV, A-PS
@@ -41,10 +44,20 @@
              WS-DISPPARM-N,
              D-MV
 
-      *    CALL COMPLEX-MULT-RETURN USING
-      *      WS-V1-COMPLEX,
-      *      WS-V2-COMPLEX,
-      *      WS-MULTPARM-OUT-COMPLEX
+           MOVE '1' TO WS-DISPPARM-N
+           CALL GA-DISPLAY USING
+             WS-DISPPARM-N,
+             ONE-MV
+
+           CALL GA-MULT-RETURN USING
+             A-MV,
+             ONE-MV,
+             E-MV
+
+           MOVE 'E' TO WS-DISPPARM-N
+           CALL GA-DISPLAY USING
+             WS-DISPPARM-N,
+             E-MV
 
            GOBACK
            .
