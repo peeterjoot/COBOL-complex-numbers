fdb369bae0aafc7cfd216ec0a991e7986812a0ae
Peeter Joot <peeterjoot@pm.me>
2023-12-31 05:05:29 +0100

    Add GAMSCAL.cob

---
+++ GA20.v0.1/src/GAMSCAL.cob
@@ -0,0 +1,24 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.    GAMSCAL.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       LINKAGE SECTION.
+        COPY MV    REPLACING ==(PRFX)== BY ==L-==.
+        COPY FLOAT REPLACING ==(PRFX)== BY ==L-==.
+       PROCEDURE DIVISION USING
+        L-MV,
+        L-V.
+      ******************************************************************
+      * @brief GA20-MKSCALAR-MODIFY
+      *
+      * @param [out] L-MV
+      *   with members ...
+      * @param [in] L-V
+      *
+           MOVE L-V TO L-SC
+           MOVE 0 TO L-GRADE
+
+           GOBACK
+           .
+
+      * vim: et ts=4 sw=4
--- GA20.v0.1/src/MULT.cob
+++ GA20.v0.1/src/MULT.cob
@@ -4,6 +4,7 @@
        WORKING-STORAGE SECTION.
        COPY GA20.
        COPY MV REPLACING ==(PRFX)== BY ==A-==.
+       COPY MV REPLACING ==(PRFX)== BY ==B-==.
        COPY GADISP REPLACING ==(PRFX)== BY ==WS-DISPPARM-==.
        PROCEDURE DIVISION.
 
@@ -13,15 +14,22 @@
            MOVE 3 TO A-SC
            MOVE 4 TO A-PS
 
-      *    CALL COMPLEX-MULT-RETURN USING
-      *      WS-V1-COMPLEX,
-      *      WS-V2-COMPLEX,
-      *      WS-MULTPARM-OUT-COMPLEX
+           CALL GA-MKSCALAR-MODIFY USING B-MV, A-SC
 
            MOVE 'A' TO WS-DISPPARM-N
            CALL GA-DISPLAY USING
              WS-DISPPARM-N,
              A-MV
+
+           MOVE 'B' TO WS-DISPPARM-N
+           CALL GA-DISPLAY USING
+             WS-DISPPARM-N,
+             B-MV
+
+      *    CALL COMPLEX-MULT-RETURN USING
+      *      WS-V1-COMPLEX,
+      *      WS-V2-COMPLEX,
+      *      WS-MULTPARM-OUT-COMPLEX
 
            GOBACK
            .
